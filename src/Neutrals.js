ad1a3da1d88281c54a711abbb1fdff8709c1fedb
Giulio Vivian <giuliovivian@gmail.com>
2018-03-29 12:45:24 +0200

    add props for senator cards and add css class for table cards

--- src/Neutrals.js
+++ src/Neutrals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SmallCard } from './SmallCard';
+import './stylesheets/common.css'
 
 export class Neutrals extends React.Component {
 
@@ -15,26 +16,49 @@ export class Neutrals extends React.Component {
                     if (playerIndex === '0') {
                         return '';
                     }
-
-                    return (
-                        <div key={playerIndex} className="col-sm-6">
-                            <p>Player: {player.name} <small>faction talents: {player.talents}</small></p>
-                            <p>Table Cards</p>
-                            <ul>
+                    
+                    let tableCardsContainer = player.tableCards && player.tableCards.length > 0 ? 
+                        <div className="tableCardsContainer" >
+                            <h6 className="align-center">Table Cards</h6>
+                            <table className="tableCards">
+                                <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Type</th>
+                                    <th>Name</th>
+                                    <th>Talents</th>
+                                    <th>Popularity</th>
+                                    <th>Influence</th>
+                                    <th>Military</th>
+                                    <th>Oratory</th>
+                                    <th>Loyalty</th>
+                                </tr>
                                 {player.tableCards.map(card => {
                                     return (
                                         <SmallCard {...card} key={card.id}></SmallCard>
                                     )
                                 })}
-                            </ul>
-                            <p>Hand Cards</p>
-                            <ul>
-                                {player.hand.map(card => {
-                                    return (
-                                        <SmallCard {...card} key={card.id}></SmallCard>
-                                    )
-                                })}
-                            </ul>
+                                </tbody>
+                            </table>
+                        </div> :
+                        <div></div>;
+                                            
+                    let handCardsContainer = player.hand && player.hand.length > 0 ? 
+                        <div className="handCardsContainer">
+                            <h6 className="align-center">Hand Cards</h6>
+                            {player.hand.map(card => {
+                                return (
+                                    <SmallCard {...card} key={card.id}></SmallCard>
+                                )
+                            })}
+                        </div> :
+                        <div></div>;
+                    
+                    return (
+                        <div style={{border: "2px solid blue", margin: "0 10px 10px 10px"}} key={playerIndex} className="col-sm-5">
+                            <p>Player: {player.name} <small>faction talents: {player.talents}</small></p>
+                            {tableCardsContainer}
+                            {handCardsContainer}
                         </div>
                     )
                 })}
--- src/SmallCard.js
+++ src/SmallCard.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export const SmallCard = (props) => (
-    <div>
-        [{props.id}] - {props.type} - {props.name} - {props.loyalty} - {props.talents} 
-    </div>
+    <tr>
+        <td>[{("0" + props.id).slice(-2)}]</td>
+        <td>{props.type}</td>
+        <td>{props.name}</td>
+        <td>{props.talents}</td>
+        <td>{props.popularity}</td>
+        <td>{props.influence}</td>
+        <td>{props.military}</td>
+        <td>{props.oratory}</td>
+        <td>{props.loyalty}</td>
+    </tr>
+
 )
