023151c28b8af8f9a73875d56443377387cc0dd1
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2018-04-03 12:49:09 +0200

    Added factions specific rules assets

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import {CardModel} from './Card';
 import {EventDeck} from './Event';
 import GamesTable from './GamesTable';
 import PopulationTable from './PopulationTable';
+import MilitaryPlan from './MilitaryPlan';
 
 import RevenueBot from './RevenueBot';
 import ForumBot from './ForumBot';
@@ -459,19 +460,23 @@ const Ror = Game({
 
       } else {
 
-        if (game.republic.activeWars.length > 1 && Util.hasAdequateForce(G).length > 1) {
+        if (game.republic.activeWars.length > 1 && Util.hasAdequateForce(game).length > 1) {
           // plan 1
-          const toFight = Util.hasAdequateForce(G);
-        } else if (Util.anyDangerousWar(G) && Util.hasAdequateForce(G).length > 0) {
+          const toFight = Util.hasAdequateForce(game);
+          game = MilitaryPlan.applyPlan1(game, ctx);
+        } else if (Util.anyDangerousWar(game) && Util.hasAdequateForce(game).length > 0) {
           // paln 2
           // intersect di Utils.anyDangerousWar(G) e Util.hasAdequateForce(G)
           // prendo il primo elemento
-        } else if (game.republic.activeWars.length > 0 && Util.hasAdequateForce(G).length === 1) {
+          game = MilitaryPlan.applyPlan2(game, ctx);
+        } else if (game.republic.activeWars.length > 0 && Util.hasAdequateForce(game).length === 1) {
           // plan3
           // controllare che Util.hasAdequateForce(G)[0] sia inclusa in game.republic.activeWars
-        } else if (game.republic.inactiveWars.length > 0 && Util.hasAdequateForce(G).length === 1) {
+          game = MilitaryPlan.applyPlan3(game, ctx);
+        } else if (game.republic.inactiveWars.length > 0 && Util.hasAdequateForce(game).length === 1) {
           // plan4
           // controllare che Util.hasAdequateForce(G)[0] sia inclusa in game.republic.inactiveWars
+          game = MilitaryPlan.applyPlan4(game, ctx);
         }
 
 
---
+++ src/MilitaryPlan.js
@@ -0,0 +1,19 @@
+export default class MilitaryPlan {
+
+    static applyPlan1(G, ctx) {
+
+    }
+
+    static applyPlan2(G, ctx) {
+
+    }
+
+    static applyPlan3(G, ctx) {
+
+    }
+
+    static applyPlan4(G, ctx) {
+        
+    }
+
+}
